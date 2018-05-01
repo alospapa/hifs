9a63163e452f89e90d471b6910bb2b300a311dec
Hectoria Danso Bediako <hectoriadansobediako@Hectorias-MacBook-Air.local>
2018-04-30 20:58:13 -0500

    completed reveal-on-scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 
 var mobileMobile = new MobileMenu();
+new RevealOnScroll($(".believe-item"), "85%");
+new RevealOnScroll($(".testimonial"), "1%");

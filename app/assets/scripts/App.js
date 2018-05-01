9c92f8cdd612eb483523b8ae55e663db292122fc
Hectoria Danso Bediako <hectoriadansobediako@Hectorias-MacBook-Air.local>
2018-04-30 22:38:54 -0500

    complete sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import $ from 'jquery';
+
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 
 var mobileMobile = new MobileMenu();
-new RevealOnScroll($(".believe-item"), "85%");
-new RevealOnScroll($(".testimonial"), "1%");
+new RevealOnScroll($(".believe-item"), "20%");
+new RevealOnScroll($(".testimonial"), "100%");
+var stickyHeader = new StickyHeader();
